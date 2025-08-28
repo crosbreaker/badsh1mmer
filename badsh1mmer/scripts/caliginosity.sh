7670fcde976d2976e4d7db4c87d573fe4350e0bc
HarryJarry <148587892+HarryJarry1@users.noreply.github.com>
2025-08-28 13:59:27 -0400

    :whale:

--- badsh1mmer/scripts/caliginosity.sh
+++ badsh1mmer/scripts/caliginosity.sh
@@ -5,21 +5,12 @@ echo ""
 echo "THIS WILL RE-ENROLL YOUR CHROMEBOOK, ATTEMPT TO FIX GBB FLAGS, DISABLE USB BOOT, AND BLOCK DEVMODE."
 echo "THIS SCRIPT ASSUMES YOU ARE USING STOCK FIRMWARE, AND HAVE WRITE-PROTECT OFF"
 echo ""
-echo "ARE YOU SURE YOU WANT TO DO THIS? [y/N]"
-
-read -re input
-
-if [ "$input" = "y" ]; then
-	vpd -i RW_VPD -s check_enrollment=1 -s block_devmode=1
-	crossystem block_devmode=1
-	crossystem dev_boot_usb=0
-	crossystem disable_dev_request=1
-	#doesnt exist tpm_manager_client take_ownership
-	#wont work, no cryptohome.  cryptohome --action=set_firmware_management_parameters --flags=0x01
-	#must use futility/usr/share/vboot/bin/set_gbb_flags.sh 0x0
-        futility gbb -s --flash --flags=0x0
-	exit
-else
-	echo "ABORT"
-	exit
-fi
+vpd -i RW_VPD -s check_enrollment=1 -s block_devmode=1
+crossystem block_devmode=1
+crossystem dev_boot_usb=0
+crossystem disable_dev_request=1
+#doesnt exist tpm_manager_client take_ownership
+#wont work, no cryptohome.  cryptohome --action=set_firmware_management_parameters --flags=0x01
+#must use futility/usr/share/vboot/bin/set_gbb_flags.sh 0x0
+futility gbb -s --flash --flags=0x0
+exit
