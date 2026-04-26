9d7c2ead0de61b4ddddce4af87e89d744eb09dc2
Harry Jarry <148587892+HarryJarry1@users.noreply.github.com>
2026-04-26 13:04:10 -0400

    very smart touchdev

--- badsh1mmer/scripts/touchdev.sh
+++ badsh1mmer/scripts/touchdev.sh
@@ -33,10 +33,26 @@ get_internal() {
   esac
 }
 mountlvm(){
-     vgchange -ay #active all volume groups
-     volgroup=$(vgscan | grep "Found volume group" | awk '{print $4}' | tr -d '"')
-     echo "found volume group:  $volgroup"
-     mount "/dev/$volgroup/unencrypted" /stateful || fail "couldnt mount p1 or lvm group.  Please recover"
+  mkdir -p /localroot 
+	mount ${intdis_prefix}$(get_booted_rootnum) /localroot -o ro
+	# yes i know below is really inefficient, but the other way isnt work and i cant understand why
+	sync # call just in case?
+	mount --bind /dev /localroot/dev
+	mount --bind /proc /localroot/proc
+	mount --bind /sys /localroot/sys
+	mount --bind /run /localroot/run
+	chroot /localroot /sbin/vgchange -ay
+  volgroup=$(chroot /localroot /sbin/vgscan | grep "Found volume group" | awk '{print $4}' | tr -d '"')
+	if [ -b "/dev/$volgroup/unencrypted" ]; then
+		echo "found volume group: $volgroup"
+		mkdir "$stateful_mount"
+		chroot /localroot /sbin/mkfs.ext4 -F /dev/$volgroup/unencrypted
+    mount /dev/$volgroup/unencrypted "$stateful_mount"
+	else
+		echo "lvm fail, falling back on p1"
+		chroot /localroot /sbin/mkfs.ext4 -F "$intdis_prefix"1 || fail "no stateful could be found/wiped"
+    mount "$intdis_prefix"1 "$stateful_mount"
+	fi
 }
 get_internal
 mount "$intdis$intdis_prefix"1 /stateful || mountlvm
